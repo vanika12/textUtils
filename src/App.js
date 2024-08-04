599b264dfc8ba01d90c8bafe92e668f158ccf74f
Vanika <155776932+vanika12@users.noreply.github.com>
2024-08-04 23:19:12 +0530

    changes
    added some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+// import { Link } from 'react-router-dom';
+// import {useState}from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import About from './Components/About'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { Route, Switch } from "react-router";
+import {
+  // BrowserRouter as Router,
+  Switch,
+  // Route
+} from "react-router-dom";
 
+ 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+ 
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <BrowserRouter>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <div className="container my-3">
+    <Routes>
+           <Route path="/About" element={<About/>}/>
+          <Route path="/" element={<TextForm/>}/>
+            {/* <TextForm  heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/> */}
+     </Routes>
     </div>
+    </BrowserRouter>
+    </> 
   );
 }
 
